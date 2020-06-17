ebec92abb00e373fd47ade78782ae37d1d38610d
Eric Hsu <clhsu123@gmail.com>
2020-06-17 17:13:48 +0800

    upload the backtracking algorithm for solution

--- solver.py
+++ solver.py
@@ -1,3 +1,19 @@
 # this is a sudoku solver
 
-print('test git')
+def solve(board):
+    find = find_empty(board)
+    if not find: #if there is no more empty space, meaning we have found the solution
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10): # Iterate i from 1 to 9
+        if valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+     return False   
