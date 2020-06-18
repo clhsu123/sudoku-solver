0a48e5e88ed60354b63bd409ecaf389e76e9893a
Eric Hsu <clhsu123@gmail.com>
2020-06-18 22:00:18 +0800

    add new function that check the available input at given position on the board

--- solver.py
+++ solver.py
@@ -24,4 +24,28 @@ def find_empty(board):
             if board[i][j] == 0:
                 return (i, j)
 
+def valid(board, num, pos):
+    # Check row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and i!=pos.[1]:
+            return False
+    
+    # Check column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and i!=pos[0]:
+            return False
+
+    # Check box
+    row_box = pos[0] // 3
+    col_box = pos[1] // 3
+
+    for i in range(row_box*3, row_box*3+3):
+        for j in range(col_box*3, col_box*3+3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+    
+    return True
+
+        
+
       
